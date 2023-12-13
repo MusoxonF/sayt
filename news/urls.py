e74a55bd252a63c34c857b903c11df3ad5d0cefc
Musoxon <orinboyevmusoxon05@gamil.com>
2023-12-13 14:32:13 +0500

    changed 13.12.2023

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import Create_news, edit, delete, detail
+from .views import Create_news, Edit, delete, detail
 
 urlpatterns = [
 	path('Create_news/', Create_news.as_view(), name='create'),
-	path('edit/<int:id>', edit, name='edit'),
+	path('edit/<int:id>', Edit.as_view(), name='edit'),
 	path('delete/<int:id>', delete, name='delete'),
 	path('detail/<int:id>', detail, name="detail"),
 
