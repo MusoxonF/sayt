9eba03b15354bd11d4af4a3592cb2b22429d9131
Musoxon <orinboyevmusoxon05@gamil.com>
2023-12-17 01:28:08 +0500

    hide password

--- user/forms.py
+++ user/forms.py
@@ -14,4 +14,4 @@ class UserForm(forms.ModelForm):
 
 class LoginForm(forms.Form):
 	username = forms.CharField(max_length=15)
-	password = forms.CharField(max_length=20)
+	password = forms.CharField(max_length=32, widget=forms.PasswordInput)
